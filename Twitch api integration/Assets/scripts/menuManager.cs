f010fac7b5ef06ec98d9778c3fdcfc368d459d52
emrecds <seyrelmeyen2@outlook.com>
2022-08-12 20:51:33 +0300

    fixes

--- Twitch api integration/Assets/scripts/menuManager.cs
+++ Twitch api integration/Assets/scripts/menuManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class menuManager : MonoBehaviour
 {
-
+    public playerInformations informations;
     void Start()
     {
         
--- Twitch api integration/Assets/scripts/playerInformations.cs
+++ Twitch api integration/Assets/scripts/playerInformations.cs
@@ -7,28 +7,48 @@ using UnityEngine.UIElements;
 public class playerInformations : MonoBehaviour
 {
 
-    public TMP_InputField oAuth;
+    public TMP_InputField oAuthInputFýeld;
+    public TMP_Text oAuthText;
+    public string oAuthString;
+    public GameObject buttons;
+    public GameObject informations;
+    public TMP_Text channelNameText;
     void Start()
     {
-        oAuth.contentType = TMP_InputField.ContentType.Password;
+        oAuthString = PlayerPrefs.GetString("oAuth");
+
+        oAuthInputFýeld.contentType = TMP_InputField.ContentType.Password;
     }
 
 
     public void contentTypeButton()
     {
-        if (oAuth.contentType == TMP_InputField.ContentType.Password)
+        if (oAuthInputFýeld.contentType == TMP_InputField.ContentType.Password)
         {
-            oAuth.contentType = TMP_InputField.ContentType.Standard;
+            oAuthInputFýeld.contentType = TMP_InputField.ContentType.Standard;
         }
 
-        else if (oAuth.contentType == TMP_InputField.ContentType.Standard)
+        else if (oAuthInputFýeld.contentType == TMP_InputField.ContentType.Standard)
         {
-            oAuth.contentType = TMP_InputField.ContentType.Password;
+            oAuthInputFýeld.contentType = TMP_InputField.ContentType.Password;
         }
     }
-    // Update is called once per frame
-    void Update()
+  
+
+    public void playerprefsSave()
     {
-        
+        oAuthString = oAuthText.text;
+        PlayerPrefs.SetString("channel", channelNameText.text);
+        PlayerPrefs.SetString("oAuth", oAuthString);
+        PlayerPrefs.Save();
     }
+
+    public void continueButton()
+    {
+       playerprefsSave();
+        buttons.SetActive(true);
+        informations.SetActive(false);
+    }
+
+
 }
